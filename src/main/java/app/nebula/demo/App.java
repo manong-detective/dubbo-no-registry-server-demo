b1ee590343f8b2eebc32b3d9b14224c6a20c76d5
陈云 <chenyun@chenyundeMacBook-Pro.local>
2024-05-15 16:46:34 +0800

    添加mf

--- src/main/java/app/nebula/demo/App.java
+++ src/main/java/app/nebula/demo/App.java
@@ -1,12 +1,10 @@
 package app.nebula.demo;
 
 import app.nebula.demo.api.GreetingsService;
-import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.bootstrap.DubboBootstrap;
 import org.apache.dubbo.config.bootstrap.builders.ApplicationBuilder;
 import org.apache.dubbo.config.bootstrap.builders.ProtocolBuilder;
 import org.apache.dubbo.config.bootstrap.builders.ServiceBuilder;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Date;
 
@@ -14,8 +12,7 @@ import java.util.Date;
  * Hello world!
  *
  */
-@SpringBootApplication
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
