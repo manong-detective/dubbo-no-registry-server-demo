e69d6aa5fc5b9b088a7ef719d67ea54d33c9a741
liyiting <liyiting@lizhi.fm>
2024-05-15 14:21:59 +0800

    增加日志

--- src/main/java/app/nebula/demo/GreetingsServiceImpl.java
+++ src/main/java/app/nebula/demo/GreetingsServiceImpl.java
@@ -5,6 +5,7 @@ import app.nebula.demo.api.GreetingsService;
 public class GreetingsServiceImpl implements GreetingsService {
     @Override
     public String sayHi(String name) {
+        System.out.println("receive req, name: " + name);
         return "hello world " + name;
     }
 }
